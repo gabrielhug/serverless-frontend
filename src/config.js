eb16c385a9a0f2a2f7bb60c526d4fbb0579b8e7c
Gabriel <gabriel@Obi.localdomain>
2019-05-26 14:19:56 -0700

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "notes-app-uploads-329485"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://062osqffyk.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_cteuFwhBN",
+    APP_CLIENT_ID: "kbtlgb6b2mvbk3l5el0lp9v24",
+    IDENTITY_POOL_ID: "us-west-2:c3e4221b-88c5-4fef-889a-a30d9616baa6"
+  }
+};
